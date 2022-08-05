8a839e2d1527661da2ca4ab124c339f13313fec1
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-08-05 17:16:07 -0300

    Testing JSON project feeding.

--- src/index.js
+++ src/index.js
@@ -6,4 +6,18 @@ function switchStack(id) {
 	document.getElementById("backend").style.display = "none";
 	document.getElementById("fullstack").style.display = "none";
 	document.getElementById(id).style.display = "block";
+
+	loadProjects(id);
+}
+
+// Load projects from JSON
+function loadProjects(id) {
+	const xmlhttp = new XMLHttpRequest();
+	xmlhttp.onload = function() {
+		const myObj = JSON.parse(this.responseText);
+		//document.getElementById("demo").innerHTML = myObj;
+		console.log(myObj);
+	};
+	xmlhttp.open("GET", "projects.json");
+	xmlhttp.send();
 }
