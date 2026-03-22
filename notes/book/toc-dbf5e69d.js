3c0ff91ef11c6c27ccecedf227eae56249fdc3b1
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-22 17:56:33 -0300

    Book output.

--- notes/book/toc-dbf5e69d.js
+++ notes/book/toc-dbf5e69d.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="distributed_systems/overview.html"><strong aria-hidden="true">1.</strong> Distributed Systems</a></span><ol class="section"><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="distributed_systems/chapter_1.html"><strong aria-hidden="true">1.1.</strong> Introduction</a></span></li></ol></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="distributed_systems/overview.html"><strong aria-hidden="true">1.</strong> Distributed Systems</a></span><ol class="section"><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="distributed_systems/chapter_1.html"><strong aria-hidden="true">1.1.</strong> Introduction</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="distributed_systems/chapter_3.html"><strong aria-hidden="true">1.2.</strong> 3.2 Virtualization</a></span></li></ol></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split('#')[0].split('?')[0];
         if (current_page.endsWith('/')) {
